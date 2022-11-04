6bd90e5632be3bb10101fc56e404ecaded78dc81
Rifkhan Mohamed <rifkhanabdullah@gmail.com>
2022-11-04 14:26:08 +0530

    first commit

---
+++ src/main/java/com/example/vowelCounter/VowelCounterApplication.java
@@ -0,0 +1,13 @@
+package com.example.vowelCounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VowelCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VowelCounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vowelCounter/VowelCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vowelCounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VowelCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
